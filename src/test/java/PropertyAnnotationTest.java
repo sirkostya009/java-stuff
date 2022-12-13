857e5e913cc242c16824ee8396fec27c5eb89a5a
Constantine <kosta.tovstik@gmail.com>
2022-12-13 13:34:12 +0200

    Simplified tests a little

--- src/test/java/PropertyAnnotationTest.java
+++ src/test/java/PropertyAnnotationTest.java
@@ -1,20 +1,21 @@
 import org.junit.jupiter.api.Test;
 import org.sirkostya009.task2.PropertyWrapper;
-import org.sirkostya009.task2.Utility;
 
+import java.net.URISyntaxException;
 import java.nio.file.Path;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
 import java.time.format.DateTimeParseException;
+import java.util.Objects;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.sirkostya009.task2.Utility.loadFromProperties;
 
 public class PropertyAnnotationTest {
     @Test
     public void testLoadFromProperties() throws Exception {
-        var resourcePath = Path.of(PropertyAnnotationTest.class.getResource("some.properties").toURI());
-        var wrapper = Utility.loadFromProperties(PropertyWrapper.class, resourcePath);
+        var wrapper = loadFromProperties(PropertyWrapper.class, path("some.properties"));
 
         assertThat(wrapper.getStringProperty()).isEqualTo("value1");
         assertThat(wrapper.getIntProperty()).isEqualTo(10);
@@ -23,19 +24,15 @@ public class PropertyAnnotationTest {
     }
 
     @Test
-    public void testFaultyDate() throws Exception {
-        var resourcePath = Path.of(PropertyAnnotationTest.class.getResource("faulty_date.properties").toURI());
-
-        assertThatThrownBy(() -> Utility.loadFromProperties(PropertyWrapper.class, resourcePath))
+    public void testFaultyDate() {
+        assertThatThrownBy(() -> loadFromProperties(PropertyWrapper.class, path("faulty_date.properties")))
                 .hasMessageContaining("01.01.1980")
                 .isInstanceOf(DateTimeParseException.class);
     }
 
     @Test
     public void testEmpty() throws Exception {
-        var resourcePath = Path.of(PropertyAnnotationTest.class.getResource("empty.properties").toURI());
-
-        var wrapper = Utility.loadFromProperties(PropertyWrapper.class, resourcePath);
+        var wrapper = loadFromProperties(PropertyWrapper.class, path("empty.properties"));
 
         assertThat(wrapper.getStringProperty()).isNull();
         assertThat(wrapper.getTimeProperty()).isNull();
@@ -43,11 +40,13 @@ public class PropertyAnnotationTest {
     }
 
     @Test
-    public void testFaultyNumber() throws Exception {
-        var resourcePath = Path.of(PropertyAnnotationTest.class.getResource("faulty_number.properties").toURI());
-
-        assertThatThrownBy(() -> Utility.loadFromProperties(PropertyWrapper.class, resourcePath))
+    public void testFaultyNumber() {
+        assertThatThrownBy(() -> loadFromProperties(PropertyWrapper.class, path("faulty_number.properties")))
                 .hasMessageContaining("va")
                 .isInstanceOf(NumberFormatException.class);
+    }
+
+    private Path path(String resourceName) throws URISyntaxException {
+        return Path.of(Objects.requireNonNull(PropertyAnnotationTest.class.getResource(resourceName)).toURI());
     }
 }
