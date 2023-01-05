2c358b1b98b2025fdaceeb90e87dad571c883d51
Constantine <kosta.tovstik@gmail.com>
2023-01-05 14:51:12 +0200

    Tests

--- src/main/java/ua/sirkostya009/javastuff/service/BookServiceImpl.java
+++ src/main/java/ua/sirkostya009/javastuff/service/BookServiceImpl.java
@@ -4,12 +4,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 import ua.sirkostya009.javastuff.dao.Book;
 import ua.sirkostya009.javastuff.dto.BookInfo;
 import ua.sirkostya009.javastuff.exception.NotFoundException;
 import ua.sirkostya009.javastuff.repository.BookRepository;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 @Service
@@ -49,18 +49,17 @@ public class BookServiceImpl implements BookService {
     @Override
     @Transactional
     public Book update(Long id, BookInfo info) {
-        var book = findBy(id);
-
         if (info.getGenreId() != null)
-            repository.updateBookGenre(id, genreService.findBy(id));
+            try { repository.updateBookGenre(id, genreService.findBy(id)); }
+            catch (Exception ignored) {}
 
         if (info.getAuthor() != null && !info.getAuthor().isBlank())
-            repository.updateBookAuthor(id, info.getAuthor());
+            repository.updateBookAuthor(id, info.getAuthor().trim());
 
         if (info.getTitle() != null && !info.getTitle().isBlank())
-            repository.updateBookTitle(id, info.getTitle());
+            repository.updateBookTitle(id, info.getTitle().trim());
 
-        return book;
+        return findBy(id);
     }
 
     @Override
---
+++ src/test/java/ua/sirkostya009/javastuff/BookControllerTests.java
@@ -0,0 +1,171 @@
+package ua.sirkostya009.javastuff;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import ua.sirkostya009.javastuff.dao.Book;
+import ua.sirkostya009.javastuff.dao.Genre;
+import ua.sirkostya009.javastuff.dto.BookInfo;
+import ua.sirkostya009.javastuff.repository.BookRepository;
+import ua.sirkostya009.javastuff.repository.GenreRepository;
+
+import java.util.Arrays;
+import java.util.Set;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(
+        webEnvironment = SpringBootTest.WebEnvironment.MOCK,
+        classes = Main.class
+)
+@AutoConfigureMockMvc
+public class BookControllerTests {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    @Autowired
+    private GenreRepository genreRepository;
+
+    @Autowired
+    private ObjectMapper mapper;
+
+    @Before
+    public void setUp() {
+        var uncategorized = genreRepository.save(new Genre(null, "Uncategorized", Set.of()));
+        var sciFi = genreRepository.save(new Genre(null, "Sci-Fi", Set.of()));
+        bookRepository.save(new Book(null, "No category book", "Author 1", uncategorized));
+        bookRepository.save(new Book(null, "Science fiction book", "Author 2", sciFi));
+    }
+
+    @After
+    public void tearDown() {
+        bookRepository.deleteAll();
+        genreRepository.deleteAll();
+    }
+
+    @Test
+    public void testGetAllBooks() throws Exception {
+        var request = MockMvcRequestBuilders
+                .get("/api/v1/books")
+                .accept(MediaType.APPLICATION_JSON);
+
+        var response = mvc
+                .perform(request)
+                .andExpect(status().isOk())
+                .andReturn()
+                .getResponse()
+                .getContentAsString();
+
+        var actualBooks = parseObject(
+                response.substring(response.indexOf('['), response.indexOf(']')),
+                BookInfo[].class
+        );
+        var expectedBooks = bookRepository.findAll().stream().map(BookInfo::of).toList();
+
+        assertThat(Arrays.asList(actualBooks)).isEqualTo(expectedBooks);
+    }
+
+    @Test
+    public void testAddBook() throws Exception {
+        var info = new BookInfo(null, "Another sci-fi book", "Author 3", 2L);
+
+        var request = MockMvcRequestBuilders
+                .post("/api/v1/books")
+                .accept(MediaType.APPLICATION_JSON)
+                .content(mapper.writeValueAsString(info))
+                .contentType(MediaType.APPLICATION_JSON);
+
+        var json = mvc
+                .perform(request)
+                .andExpect(status().isCreated())
+                .andReturn()
+                .getResponse()
+                .getContentAsString();
+
+        var actualBook = parseObject(json, BookInfo.class);
+        var expectedBook = bookRepository.findById(3L).get();
+
+        assertThat(actualBook.getId()).isEqualTo(expectedBook.getId());
+        assertThat(actualBook.getTitle()).isEqualTo(expectedBook.getTitle());
+        assertThat(actualBook.getAuthor()).isEqualTo(expectedBook.getAuthor());
+        assertThat(actualBook.getGenreId()).isEqualTo(expectedBook.getGenre().getId());
+    }
+
+    @Test
+    public void testGetBookById() throws Exception {
+        var request = MockMvcRequestBuilders
+                .get("/api/v1/books/1")
+                .accept(MediaType.APPLICATION_JSON);
+
+        var json = mvc
+                .perform(request)
+                .andExpect(status().isOk())
+                .andReturn()
+                .getResponse()
+                .getContentAsString();
+
+        var actualBook = parseObject(json, BookInfo.class);
+        var expectedBook = bookRepository.findById(1L).get();
+
+        assertThat(actualBook.getId()).isEqualTo(expectedBook.getId());
+        assertThat(actualBook.getTitle()).isEqualTo(expectedBook.getTitle());
+        assertThat(actualBook.getAuthor()).isEqualTo(expectedBook.getAuthor());
+        assertThat(actualBook.getGenreId()).isEqualTo(expectedBook.getGenre().getId());
+    }
+
+    @Test
+    public void testUpdateBook() throws Exception {
+        var info = new BookInfo(null, "Updated Title", "Updated Author", 2L);
+
+        var request = MockMvcRequestBuilders
+                .put("/api/v1/books/1")
+                .accept(MediaType.APPLICATION_JSON)
+                .content(mapper.writeValueAsString(info))
+                .contentType(MediaType.APPLICATION_JSON);
+
+        var json = mvc
+                .perform(request)
+                .andExpect(status().isOk())
+                .andReturn()
+                .getResponse()
+                .getContentAsString();
+
+        var actualBook = parseObject(json, BookInfo.class);
+        var expectedBook = bookRepository.findById(1L).get();
+
+        assertThat(actualBook).isEqualTo(expectedBook);
+    }
+
+    @Test
+    public void testDeleteBook() throws Exception {
+        var request = MockMvcRequestBuilders
+                .delete("/api/v1/books/1")
+                .accept(MediaType.APPLICATION_JSON);
+
+        mvc.perform(request)
+                .andExpect(status().isNoContent())
+                .andReturn();
+
+        assertThat(bookRepository.existsById(1L)).isFalse();
+    }
+
+    private <T> T parseObject(String json, Class<T> clazz) throws JsonProcessingException {
+        return mapper.readValue(json, clazz);
+    }
+}
