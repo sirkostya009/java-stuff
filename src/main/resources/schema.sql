ed0770f9f995b1fd8dc9fb67370825f86a412ce4
Constantine <kosta.tovstik@gmail.com>
2023-01-16 19:10:56 +0200

    Update schemas: remove genre_books, as it is not needed in our application.

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,4 +1,3 @@
-drop table if exists genre_books cascade;
 drop table if exists genre cascade;
 drop table if exists book cascade;
 
@@ -12,14 +11,5 @@ create table book (
     author varchar(255) not null,
     title varchar(255) not null,
     genre_id bigint,
-    constraint FK_genre_id foreign key (genre_id) references genre
-);
-
-create table genre_books (
-    genre_id bigint not null,
-    books_id bigint not null,
-    primary key (genre_id, books_id),
-    constraint UK_books_id unique (books_id),
-    constraint FK_books_id foreign key (books_id) references book,
     constraint FK_genre_id foreign key (genre_id) references genre
 );
