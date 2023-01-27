eed710ccf2bcdc43111354b2696a6dcfbb5c4e47
Constantine <kosta.tovstik@gmail.com>
2023-01-27 02:14:20 +0200

    Fixed a table

--- frontend/src/components/Numpad.jsx
+++ frontend/src/components/Numpad.jsx
@@ -13,34 +13,34 @@ class Numpad extends Component {
     } = this.props;
 
     return (
-        <div>
-          <table>
-            <tr>
-              <th><Button onClick={onNumClick} variant="outlined">1</Button></th>
-              <th><Button onClick={onNumClick} variant="outlined">2</Button></th>
-              <th><Button onClick={onNumClick} variant="outlined">3</Button></th>
-              <th><Button onClick={onButtonClick} variant="outlined">/</Button></th>
-            </tr>
-            <tr>
-              <th><Button onClick={onNumClick} variant="outlined">4</Button></th>
-              <th><Button onClick={onNumClick} variant="outlined">5</Button></th>
-              <th><Button onClick={onNumClick} variant="outlined">6</Button></th>
-              <th><Button onClick={onButtonClick} variant="outlined">*</Button></th>
-            </tr>
-            <tr>
-              <th><Button onClick={onNumClick} variant="outlined">7</Button></th>
-              <th><Button onClick={onNumClick} variant="outlined">8</Button></th>
-              <th><Button onClick={onNumClick} variant="outlined">9</Button></th>
-              <th><Button onClick={onButtonClick} variant="outlined">-</Button></th>
-            </tr>
-            <tr>
-              <th></th>
-              <th><Button onClick={onNumClick} variant="outlined">0</Button></th>
-              <th><Button onClick={onButtonClick} variant="outlined">=</Button></th>
-              <th><Button onClick={onButtonClick} variant="outlined">+</Button></th>
-            </tr>
-          </table>
-        </div>
+      <table>
+        <tbody>
+          <tr>
+            <td><Button onClick={onNumClick} variant="outlined">1</Button></td>
+            <td><Button onClick={onNumClick} variant="outlined">2</Button></td>
+            <td><Button onClick={onNumClick} variant="outlined">3</Button></td>
+            <td><Button onClick={onButtonClick} variant="outlined">/</Button></td>
+          </tr>
+          <tr>
+            <td><Button onClick={onNumClick} variant="outlined">4</Button></td>
+            <td><Button onClick={onNumClick} variant="outlined">5</Button></td>
+            <td><Button onClick={onNumClick} variant="outlined">6</Button></td>
+            <td><Button onClick={onButtonClick} variant="outlined">*</Button></td>
+          </tr>
+          <tr>
+            <td><Button onClick={onNumClick} variant="outlined">7</Button></td>
+            <td><Button onClick={onNumClick} variant="outlined">8</Button></td>
+            <td><Button onClick={onNumClick} variant="outlined">9</Button></td>
+            <td><Button onClick={onButtonClick} variant="outlined">-</Button></td>
+          </tr>
+          <tr>
+            <td></td>
+            <td><Button onClick={onNumClick} variant="outlined">0</Button></td>
+            <td><Button onClick={onButtonClick} variant="outlined">=</Button></td>
+            <td><Button onClick={onButtonClick} variant="outlined">+</Button></td>
+          </tr>
+        </tbody>
+      </table>
     );
   }
 }
