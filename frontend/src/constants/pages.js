53f57acb855328643fe7d5ee0a46d2c7e2d88112
Constantine <kosta.tovstik@gmail.com>
2023-02-10 15:13:13 +0200

    Added fe for development purposes.

---
+++ frontend/src/constants/pages.js
@@ -0,0 +1,4 @@
+export const INITIAL = 'initial';
+export const LOGIN = 'login';
+export const ENTITIES = 'entities';
+export const EDIT = 'edit';
---
+++ frontend/src/pages/Initial/reducers/reducer.js
@@ -0,0 +1,14 @@
+const initialState = {
+  availableItems: [
+    'як+вивчити+js',
+    'entities',
+    'Картинка',
+    'anotherPath',
+  ],
+};
+
+export default (state = initialState, {type, payload}) => {
+  switch (type) {
+    default: return state;
+  }
+}
