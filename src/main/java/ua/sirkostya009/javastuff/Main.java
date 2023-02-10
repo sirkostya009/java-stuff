a6cc270e3f3a4cb61ec6b85ca93ba899c1249e5a
Constantine <kosta.tovstik@gmail.com>
2023-02-10 15:18:52 +0200

    Added cors configuration

--- src/main/java/ua/sirkostya009/javastuff/Main.java
+++ src/main/java/ua/sirkostya009/javastuff/Main.java
@@ -4,6 +4,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import ua.sirkostya009.javastuff.model.NamedEntity;
 import ua.sirkostya009.javastuff.repository.EntityRepository;
 
@@ -25,5 +28,22 @@ public class Main {
 
             repository.saveAll(List.of(entity1, entity2, entity3, entity4));
         };
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        var config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.setAllowedOrigins(List.of("http://localhost:4200", "http://localhost:3000"));
+        config.setAllowedHeaders(List.of(
+                "Origin", "Access-Control-Allow-Origin", "Content-Type", "Accept", "Authorization", "Origin, Accept",
+                "X-Requested-With", "Access-Control-Request-Method", "Access-Control-Request-Headers"
+        ));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+
+        var configSource = new UrlBasedCorsConfigurationSource();
+        configSource.registerCorsConfiguration("/**", config);
+
+        return new CorsFilter(configSource);
     }
 }
