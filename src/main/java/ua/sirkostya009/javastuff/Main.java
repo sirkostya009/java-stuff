6c0541e1df092396460f15dba10c48a6f44a5c87
Constantine <kosta.tovstik@gmail.com>
2023-02-15 12:16:07 +0200

    Delete shit

--- src/main/java/ua/sirkostya009/javastuff/Main.java
+++ src/main/java/ua/sirkostya009/javastuff/Main.java
@@ -1,49 +1,13 @@
 package ua.sirkostya009.javastuff;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
-import ua.sirkostya009.javastuff.model.NamedEntity;
-import ua.sirkostya009.javastuff.repository.EntityRepository;
-
-import java.util.List;
 
 @SpringBootApplication
 public class Main {
+
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
 
-    @Bean
-    public CommandLineRunner runner(EntityRepository repository) {
-        return args -> {
-            var entity1 = new NamedEntity(1L, "asd");
-            var entity2 = new NamedEntity(2L, "123");
-            var entity3 = new NamedEntity(3L, "456");
-            var entity4 = new NamedEntity(4L, "fgh");
-
-            repository.saveAll(List.of(entity1, entity2, entity3, entity4));
-        };
-    }
-
-    @Bean
-    public CorsFilter corsFilter() {
-        var config = new CorsConfiguration();
-        config.setAllowCredentials(true);
-        config.setAllowedOrigins(List.of("http://localhost:4200", "http://localhost:3000"));
-        config.setAllowedHeaders(List.of(
-                "Origin", "Access-Control-Allow-Origin", "Content-Type", "Accept", "Authorization", "Origin, Accept",
-                "X-Requested-With", "Access-Control-Request-Method", "Access-Control-Request-Headers"
-        ));
-        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-
-        var configSource = new UrlBasedCorsConfigurationSource();
-        configSource.registerCorsConfiguration("/**", config);
-
-        return new CorsFilter(configSource);
-    }
 }
--- src/main/java/ua/sirkostya009/javastuff/repository/EntityRepository.java
+++
@@ -1,7 +0,0 @@
-package ua.sirkostya009.javastuff.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ua.sirkostya009.javastuff.model.NamedEntity;
-
-public interface EntityRepository extends JpaRepository<NamedEntity, Long> {
-}
