1a2ed396f19daa1731de063526cd21a22aa51cdd
Constantine <kosta.tovstik@gmail.com>
2023-02-20 15:45:18 +0200

    Updated CouldNotBeParsed code to 500.

--- src/main/java/ua/sirkostya009/javastuff/exception/CouldNotBeParsed.java
+++ src/main/java/ua/sirkostya009/javastuff/exception/CouldNotBeParsed.java
@@ -5,6 +5,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @StandardException
-@ResponseStatus(code = HttpStatus.BAD_REQUEST)
+@ResponseStatus(code = HttpStatus.INTERNAL_SERVER_ERROR)
 public class CouldNotBeParsed extends RuntimeException {
 }
