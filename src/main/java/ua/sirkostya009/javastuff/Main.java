cc3eab7a760ef75ab98bb5594a550d9f3647944d
Constantine <kosta.tovstik@gmail.com>
2023-03-01 17:52:19 +0200

    Deleted everything. Updated pom.xml.

--- src/main/java/ua/sirkostya009/javastuff/Main.java
+++ src/main/java/ua/sirkostya009/javastuff/Main.java
@@ -2,11 +2,8 @@ package ua.sirkostya009.javastuff;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
-import ua.sirkostya009.javastuff.repository.PublicFigureRepository;
 
 @SpringBootApplication
-@EnableMongoRepositories(basePackageClasses = PublicFigureRepository.class)
 public class Main {
 
     public static void main(String[] args) {
